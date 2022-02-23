6746b6d9b6ad6653e0171a8f9098ba2aaaf53db6
Unknown <panos@georgantas.info>
2022-02-23 12:17:13 +0200

    [antikleia] deploy

--- src/conf/global.config.js
+++ src/conf/global.config.js
@@ -33,7 +33,7 @@ var settings = {
 if (window.location.hostname === 'localhost') {
   // settings.baseUrl = process.env.WITH_BASE_URL;  // Original WITH-backend
   // settings.baseUrl = process.env.DEV_BASE_URL;   // Backend with test-DB
-  // settings.baseUrl = process.env.LOCAL_BASE_URL; // Local backend for testing
+  settings.baseUrl = process.env.LOCAL_BASE_URL; // Local backend for testing
   // settings.project = 'WITHcrowd';
   settings.logLevel = 4; // Debug
 }
@@ -55,23 +55,23 @@ export default settings;
 */
 
 var settings = {
-  project: 'CrowdHeritage',
-  space: 'espace',
+  project: process.env.PROJECT,
+  space: process.env.SPACE,
   auth: {
-    google: '',
-    facebook: ''
+    google: process.env.WITH_GOOGLE_SECRET,
+    facebook: process.env.WITH_FACEBOOK_SECRET
   },
-  baseUrl: 'https://api.crowdheritage.eu', // Production backend
-  apiUrl: '/assets/developers-lite.html',
-  googlekey: '',
+  baseUrl: process.env.BASE_URL, // Production backend
+  apiUrl: process.env.API_URL,
+  googlekey: process.env.WITH_GOOGLE_KEY,
   logLevel: 1 // Error: 1, Warn: 2, Info: 3, Debug: 4
 };
 
-try {
-  var localSettings = require('./local.config.js');
-  $.extend(true, settings, localSettings.settings);
-} catch (err) {
-  console.log("Local configuration file not available");
-}
+// try {
+//   var localSettings = require('./local.config.js');
+//   $.extend(true, settings, localSettings.settings);
+// } catch (err) {
+//   console.log("Local configuration file not available");
+// }
 
 export default settings;
